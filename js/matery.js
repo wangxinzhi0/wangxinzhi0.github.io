90e3ce09e0c5aa0213970ac23cc8d79dd5651fd0
wangxinzhi0 <xinzhi_wang0@163.com>
2021-05-28 08:23:18 +0800

    Site updated: 2021-05-28 08:23:17

--- js/matery.js
+++ js/matery.js
@@ -146,27 +146,30 @@ $(function () {
         }
     }
 
-    	
-	$(".nav-menu>li").hover(function(){
-		$(this).children('ul').stop(true,true).show();
-		 $(this).addClass('nav-show').siblings('li').removeClass('nav-show');
-		
-	},function(){
-		$(this).children('ul').stop(true,true).hide();
-		$('.nav-item.nav-show').removeClass('nav-show');
-	})
-	
-    $('.m-nav-item>a').on('click',function(){
-            if ($(this).next('ul').css('display') == "none") {
-                $('.m-nav-item').children('ul').slideUp(300);
-                $(this).next('ul').slideDown(100);
-                $(this).parent('li').addClass('m-nav-show').siblings('li').removeClass('m-nav-show');
-            }else{
-                $(this).next('ul').slideUp(100);
-                $('.m-nav-item.m-nav-show').removeClass('m-nav-show');
-            }
+
+    $(".nav-menu>li").hover(function () {
+        $(this).children('ul').stop(true, true).show();
+        $(this).addClass('nav-show').siblings('li').removeClass('nav-show');
+
+    }, function () {
+        $(this).children('ul').stop(true, true).hide();
+        $('.nav-item.nav-show').removeClass('nav-show');
+    })
+
+    $('.m-nav-item>a').on('click', function () {
+        if ($(this).next('ul').css('display') == "none") {
+            $('.m-nav-item').children('ul').slideUp(300);
+            $(this).next('ul').slideDown(100);
+            $(this).parent('li').addClass('m-nav-show').siblings('li').removeClass('m-nav-show');
+        } else {
+            $(this).next('ul').slideUp(100);
+            $('.m-nav-item.m-nav-show').removeClass('m-nav-show');
+        }
     });
 
     // 初始化加载 tooltipped.
     $('.tooltipped').tooltip();
 });
+
+// 深色模式设置
+function switchNightMode() { var body = document.body; if(body.classList.contains('dark')){ document.body.classList.remove('dark'); localStorage.setItem('dark','0'); $('#nightMode').removeClass("fa-lightbulb-o").addClass("fa-moon-o"); return; } else { document.body.classList.add('dark'); localStorage.setItem('dark','1'); $('#nightMode').removeClass("fa-moon-o").addClass("fa-lightbulb-o"); return; } }
